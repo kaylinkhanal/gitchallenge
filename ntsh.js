9b73e8f886316a2b6e35e66de6e5e06b76142dc6
princesingh979 <princepk979.pp@gmail.com>
2024-04-22 10:47:50 +0545

    find missing array

--- ntsh.js
+++ ntsh.js
@@ -1,3 +1,25 @@
-const name ="nitesh"
-console.log("Hello, World!");
+// const name ="nitesh"
+// console.log("Hello, World!");
 
+// this is program to find the missing number from the given array
+
+// input : array[1,0,3]
+// output: 2
+const name="Nitesh"
+const greeting="hello"
+
+console.log(`${greeting}, ${name} your code is modify`)
+
+const arr=[1,2,3,4,5,0]
+
+const len= arr.length;
+
+let expectedSum= len*(len+1)/2;
+let actualSum=0;
+for(let i=0; i<len;i++){
+    actualSum+=arr[i]
+}
+const missingNum= expectedSum-actualSum
+console.log(expectedSum)
+console.log(actualSum)
+console.log(missingNum)
