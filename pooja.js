80b14bf559c1bbaf0df038250b566792e1b60441
Anirudh Chaudhary <anurudhchaudhary97@gmail.com>
2024-04-22 10:42:44 +0545

    find missing array

--- pooja.js
+++ pooja.js
@@ -1,2 +1,22 @@
-const arr=[3,4,7]
-arr.length
+// this is program to find the missing number from the given array
+
+// input : array[1,0,3]
+// output: 2
+const name="Pooja"
+const greeting="hello"
+
+console.log(`${greeting}, ${name} your code is modify`)
+
+const arr=[1,2,3,4,5,0]
+
+const len= arr.length;
+
+let expectedSum= len*(len+1)/2;
+let actualSum=0;
+for(let i=0; i<len;i++){
+    actualSum+=arr[i]
+}
+const missingNum= expectedSum-actualSum
+console.log(expectedSum)
+console.log(actualSum)
+console.log(missingNum)
